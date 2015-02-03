ea9fa72fbb309a39424e70513c8b859e89409c51
Douglas Manzelmann <dougamanzelmann@gmail.com>
2015-02-03 16:49:41 -0500

    Just getting started on the percolation class

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,20 +2,25 @@
  * Created by test on 2/2/15.
  */
 public class Percolation {
+    boolean[][] grid;
+    WeightedQuickUnionUF percolate;
+
     public Percolation(int N) {
+        grid = new boolean[N+1][N+1];
+        percolate = new WeightedQuickUnionUF(N+1);
 
     }
 
     public void open(int i, int j) {
-
+        grid[i][j] = true;
     }
 
     public boolean isOpen(int i, int j) {
-
+        return grid[i][j];
     }
 
     public boolean isFull(int i, int j) {
-
+        return grid[i][j];
     }
 
     public boolean percolates() {

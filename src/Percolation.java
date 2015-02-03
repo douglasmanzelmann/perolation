dd99d7e29c6890f267686e42e10e20357694572c
douglas manzelmann <dougamanzelmann@gmail.com>
2015-02-02 22:26:11 -0500

    Initial commit

---
+++ src/Percolation.java
@@ -0,0 +1,28 @@
+/**
+ * Created by test on 2/2/15.
+ */
+public class Percolation {
+    public Percolation(int N) {
+
+    }
+
+    public void open(int i, int j) {
+
+    }
+
+    public boolean isOpen(int i, int j) {
+
+    }
+
+    public boolean isFull(int i, int j) {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,16 @@
+/**
+ * Created by test on 2/2/15.
+ */
+public class PercolationStats {
+    public PercolationStats(int N, int T) {}
+
+    public double mean() {}
+
+    public double stddev() {}
+
+    public double confidenceLo() {}
+
+    public double confidenceHi() {}
+
+    public static void main(String[] args) {}
+}

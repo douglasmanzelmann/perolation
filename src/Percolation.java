31bb917e10d35c8c703e2da69c2c276daa2568f6
douglas <dougamanzelmann@gmail.com>
2015-02-03 23:06:19 -0500

    Tired...

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,17 +2,24 @@
  * Created by test on 2/2/15.
  */
 public class Percolation {
+    int size;
     boolean[][] grid;
     WeightedQuickUnionUF percolate;
 
     public Percolation(int N) {
+        size = N;
         grid = new boolean[N+1][N+1];
-        percolate = new WeightedQuickUnionUF(N+1);
+        percolate = new WeightedQuickUnionUF(N+2);
+
+        //connect virtual top row (i.e., 0) to top row
+        //connect virtual bottom row (i.e., n+1) to bottom row
+        //probably in another method
 
     }
 
     public void open(int i, int j) {
         grid[i][j] = true;
+        //also do this in the WeightedQuickUnionUF
     }
 
     public boolean isOpen(int i, int j) {
@@ -24,10 +31,19 @@ public class Percolation {
     }
 
     public boolean percolates() {
+        percolate.connected(0, size+1);
 
+        return false;
     }
 
     public static void main(String[] args) {
+        Percolation test = new Percolation(2);
 
+        System.out.println("Is open? " + test.isOpen(1, 1));
+        System.out.println("Percolates? " + test.percolates());
+
+        test.open(1, 1);
+        test.open(2, 1);
+        System.out.println("Percolates? " + test.percolates());
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,7 +2,7 @@
  * Created by test on 2/2/15.
  */
 public class PercolationStats {
-    public PercolationStats(int N, int T) {}
+    /*public PercolationStats(int N, int T) {}
 
     public double mean() {}
 
@@ -12,5 +12,12 @@ public class PercolationStats {
 
     public double confidenceHi() {}
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]); // NxN grid
+        int T = Integer.parseInt(args[1]); // T experiments
+
+        for (int i = 0; i < T; i++) {
+
+        }
+    }*/
 }
